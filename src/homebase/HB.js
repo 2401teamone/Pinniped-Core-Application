2f32f3cb119ffd9f5848590b7ad3979f714e9544
Alan Cho <alancho397@gmail.com>
2024-03-08 23:41:07 -0600

    Documentation for HB.js
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Josh Lakenan <joshualakenan@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

--- src/homebase/HB.js
+++ src/homebase/HB.js
@@ -1,8 +1,13 @@
-import initApi from '../api/init_api.js';
-import DAO from '../dao/dao.js';
-import EventEmitter from 'events';
-import { InvalidCustomRouteError } from '../utils/errors.js';
+import initApi from "../api/init_api.js";
+import DAO from "../dao/dao.js";
+import EventEmitter from "events";
+import { InvalidCustomRouteError } from "../utils/errors.js";
 
+/**
+ * HB (HomeBase) Class
+ * Runs the application of the backend.
+ * Offers extensibility of custom routes.
+ */
 class HB {
   static createApp(config) {
     return new HB(config);
@@ -13,36 +18,42 @@ class HB {
     this.events = new EventEmitter();
     this.customRoutes = [];
   }
-
+  /**
+   * Allows developers to add their own route
+   * That can receive a HTTP request to the given path.
+   * When that path is reached with the appropriate method,
+   * It'll invoke the function, handler.
+   * @param {string} method
+   * @param {string} path
+   * @param {function} handler
+   */
   addRoute(method, path, handler) {
-    const VALID_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
+    const VALID_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"];
     if (!VALID_METHODS.includes(method)) {
       throw new InvalidCustomRouteError(
         `Please provide one of the valid methods: ${VALID_METHODS}`
       );
     }
-    if (path.startsWith('/api'))
+    if (path.startsWith("/api"))
       throw new InvalidCustomRouteError(
         "Cannot use '/api' as the beginning of your route, as it is saved for pre-generated routes."
       );
-    if (typeof handler !== 'function')
+    if (typeof handler !== "function")
       throw new InvalidCustomRouteError(
-        'Please provide a function as your handler.'
+        "Please provide a function as your handler."
       );
 
     this.customRoutes.push({ method, path, handler });
   }
 
-  // useful for: enriching rows with additional properties; adding custom validation; running custom data aggregation operations and adding to res
   /**
-   * getAllRows
    * Returns an object that adds an event handler and trigger events of the type: "GET_ALL_ROWS".
-   * The callback passed to add is executed when the event is heard.
+   * The callback passed to add is executed when the event is heard on the passed in tables.
    * @param {...string} tables
    * @returns {object}
    */
   onGetAllRows(...tables) {
-    const EVENT_NAME = 'GET_ALL_ROWS';
+    const EVENT_NAME = "GET_ALL_ROWS";
     return {
       add: (handler) => {
         this.events.on(EVENT_NAME, (event) => {
@@ -62,6 +73,10 @@ class HB {
 
   onGetOneRow() {}
 
+  /**
+   * Returns the app's DAO instance.
+   * @returns {object DAO}
+   */
   getDAO() {
     return this.DAO;
   }
