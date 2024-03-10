702544767d6b0134d599dcbfeb9828d0b10787c3
Alan Cho <alancho397@gmail.com>
2024-03-10 16:35:39 -0500

    Added documentation for auth.js
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Josh Lakenan <joshualakenan@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,15 @@
+import bcrypt from "bcrypt";
 import { Router } from "express";
 import catchError from "../utils/catch_error.js";
 import { AuthenticationError } from "../utils/errors.js";
-import bcrypt from "bcrypt";
 
+/**
+ * Creates an Express Router object
+ * That sets middleware and handlers for
+ * The designated authentication routes.
+ * @param {object HB} app
+ * @returns {object Router} router
+ */
 export default function generateAuthRouter(app) {
   const router = Router();
   const authApi = new AuthApi(app);
@@ -12,20 +19,12 @@ export default function generateAuthRouter(app) {
     res.send(req.session);
   });
 
-  //register a user
   router.post("/register", catchError(authApi.registerHandler()));
-
-  //Login a user
   router.post("/login", catchError(authApi.loginHandler()));
-
-  //Register an Admin
   router.post("/admin/register", catchError(authApi.registerAdminHandler()));
-
-  //Login a Admin
   router.post("/admin/login", catchError(authApi.loginAdminHandler()));
-
-  //Logout a user
   router.post("/logout", catchError(authApi.logoutHandler()));
+
   return router;
 }
 
@@ -42,23 +41,25 @@ class AuthApi {
     return async (req, res, next) => {};
   }
 
+  /**
+   * Returns a handler function that creates a new user.
+   * It checks if the username already exists.
+   * And if not, then creates the user and saves the credentials in the 'users' table.
+   * @returns {function}
+   */
   registerHandler() {
     return async (req, res, next) => {
       const { username, password } = req.body;
 
-      //Check if username exists
+      // Checks if 'username' exists in 'users'
       const existingUser = await this.app
         .getDAO()
         .search("users", { username });
+      if (existingUser.length)
+        throw new AuthenticationError("Username not available.");
 
-      if (existingUser.length) {
-        throw new AuthenticationError("Username not available");
-      }
-
-      //hash password
+      // Hashes the inputted password and inserts this user's credentials into the 'users' table.
       const hashedPassword = await bcrypt.hash(password, 10);
-
-      //insert a new user into the database
       const createdUser = await this.app
         .getDAO()
         .createOne("users", { username, password: hashedPassword });
@@ -68,23 +69,24 @@ class AuthApi {
     };
   }
 
+  /**
+   * Returns a handler that adds an admin user to the '_admin' table.
+   * It checks if the username already exists, and if not, adds the user to the '_admin' table.
+   * @returns {function}
+   */
   registerAdminHandler() {
     return async (req, res, next) => {
       const { username, password } = req.body;
 
-      //Check if username exists
+      // Checks if 'username' exists in 'users'.
       const existingAdmin = await this.app
         .getDAO()
         .search("_admins", { username });
-
-      if (existingAdmin.length) {
+      if (existingAdmin.length)
         throw new AuthenticationError("Username not available");
-      }
 
-      //hash password
+      // Hashes the inputted password and inserts this admin's credentials into the '_admin' table.
       const hashedPassword = await bcrypt.hash(password, 10);
-
-      //insert a new user into the database
       const createdAdmin = await this.app
         .getDAO()
         .createOne("_admins", { username, password: hashedPassword });
@@ -94,72 +96,75 @@ class AuthApi {
     };
   }
 
+  /**
+   * Returns a handler that logs in a user.
+   * It checks if the credentials exist:
+   *  If the username is found in 'users',
+   *  And if the inputted password hashed matches the hashed password in 'users'.
+   * If the credentials match, then the user's session is established.
+   * @returns {function}
+   */
   loginHandler() {
     return async (req, res, next) => {
       const { username, password } = req.body;
 
-      //Check if username exists
+      // Checks if 'username' exists in 'users'.
       const existingUser = await this.app
         .getDAO()
         .search("users", { username });
+      if (!existingUser.length) throw new AuthenticationError();
 
-      if (!existingUser.length) {
-        throw new AuthenticationError();
-      }
-
+      // Hashes the inputted password, and sees if it's equal to the hashed password in 'users'.
       const match = await bcrypt.compare(password, existingUser[0].password);
+      if (!match) throw new AuthenticationError();
 
-      if (!match) {
-        throw new AuthenticationError();
-      }
+      // Sets the session's user to the found user.
+      req.session.user = existingUser[0];
+      delete req.session.user.password;
 
-      const user = existingUser[0];
-      delete user.password;
-
-      //set session user
-      req.session.user = user;
-
-      console.log("logged in user: ", user);
-
-      res.status(200).json({ message: "User logged in successfully" });
+      console.log("Logged in user: ", req.session.user);
+      res.status(200).json({ message: "User logged in successfully." });
     };
   }
 
+  /**
+   * Returns a handler that logs in an admin.
+   * It checks if the credentials for the admin exists in '_admin',
+   * And checks if the credentials inputted match the saved admin in '_admin'.
+   * If the credentials match, then the admin's session is established.
+   * @returns {function}
+   */
   loginAdminHandler() {
     return async (req, res, next) => {
       const { username, password } = req.body;
 
-      //Check if username exists
+      // Check if the 'username' exists in '_admins'.
       const existingAdmin = await this.app
         .getDAO()
         .search("_admins", { username });
+      if (!existingAdmin.length) throw new AuthenticationError();
 
-      if (!existingAdmin.length) {
-        throw new AuthenticationError();
-      }
-
+      // Hashes the inputted password and compares it with the hashed password in '_admins'.
       const match = await bcrypt.compare(password, existingAdmin[0].password);
+      if (!match) throw new AuthenticationError();
 
-      if (!match) {
-        throw new AuthenticationError();
-      }
+      // Sets the session's user to the found admin.
+      req.session.user = existingAdmin[0];
+      delete req.session.user.password;
 
-      const admin = existingAdmin[0];
-      delete admin.password;
-
-      //set session user
-      req.session.user = admin;
-
-      console.log("logged in admin: ", admin);
-
-      res.status(200).json({ message: "Admin logged in successfully" });
+      console.log("logged in admin: ", req.session.user);
+      res.status(200).json({ message: "Admin logged in successfully." });
     };
   }
 
+  /**
+   * Returns a handler that logs out the current session's user.
+   * @returns {function}
+   */
   logoutHandler() {
     return async (req, res, next) => {
       delete req.session.user;
-      res.status(200).json({ message: "User logged out" });
+      res.status(200).json({ message: "User logged out." });
     };
   }
 }
