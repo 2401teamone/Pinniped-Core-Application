55737d08deeb619c5f0a649727566090f383ca08
Josh Lakenan <joshualakenan@gmail.com>
2024-03-22 09:39:06 -0500

    README.md cleanup
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

--- index.js
+++ index.js
@@ -10,68 +10,8 @@ app.addRoute("GET", "/custom", (req, res, next) => {
 });
 
 // add event-driven functionality
-app.onGetAllRows("Animals", "users").add((event) => {
+app.onGetAllRows().add((event) => {
   console.log("Triggered event: onGetAllRows");
-});
-
-app.onGetOneRow("Animals").add((event) => {
-  console.log("Triggered event: GET ONE ROW");
-});
-
-app.onUpdateOneRow("Animals").add((event) => {
-  console.log("Triggered event: UPDATE ONE ROW");
-});
-
-app.onDeleteOneRow("Animals").add((event) => {
-  console.log("Triggered event: DELETE ONE ROW");
-});
-
-app.onCreateOneRow("Animals").add((event) => {
-  console.log("Triggered event: CREATE ONE ROW");
-});
-
-app.onBackupDatabase().add((event) => {
-  console.log("Triggered event: BACKUP_DATABASE");
-});
-
-app.onLogout().add((event) => {
-  console.log("Triggered event: LOGOUT");
-});
-
-app.onLoginUser().add((event) => {
-  console.log("Triggered event: LOGIN");
-});
-
-app.onRegisterAdmin().add((event) => {
-  console.log("Triggered event: ON_REGISTER_ADMIN");
-});
-
-app.onLoginAdmin().add((event) => {
-  console.log("Triggered event: LOGIN_ADMIN");
-});
-
-app.onRegisterUser().add((event) => {
-  console.log("Triggered event: REGISTER_USER");
-});
-
-app.onCustomRoute().add((event) => {
-  console.log("Triggered event: CUSTOM_ROUTE");
-});
-
-app.onGetTableMeta().add((event) => {
-  console.log("Triggered Event: TABLE_META_GET");
-});
-
-app.onCreateTable().add((event) => {
-  console.log("Triggered Event: CREATE TABLE");
-});
-
-app.onUpdateTable().add((event) => {
-  console.log("Triggered Event: UPDATE TABLE");
-});
-
-app.onDropTable().add((event) => {
-  console.log("Triggered Event: DROP TABLE");
 });
 
 app.start(3000);
