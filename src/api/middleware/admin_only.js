937657e1f9151a859000fc9241e5c96f44fc6815
Josh Lakenan <joshualakenan@gmail.com>
2024-03-22 09:31:47 -0500

    Updated admin_only middlewear, and turned on admin_only access to schema and admin routers
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

--- src/api/middleware/admin_only.js
+++ src/api/middleware/admin_only.js
@@ -1,11 +1,17 @@
 import catchError from "../../utils/catch_error.js";
-import { ForbiddenError } from "../../utils/errors.js";
+import { ForbiddenError, UnauthenticatedError } from "../../utils/errors.js";
 
+/**
+ *  Middleware that throws an unauthenticated error if the user is not logged in
+ *  or a forbidden error if the user is not an admin
+ *  @returns {function} middleware
+ */
 export default function adminOnly() {
   return catchError(async (req, res, next) => {
-    if (!req.session.user || req.session.user.role !== "admin") {
-      throw new ForbiddenError();
-    }
+    if (!req.session.user) throw new UnauthenticatedError();
+
+    if (req.session.user.role !== "admin") throw new ForbiddenError();
+
     next();
   });
 }
